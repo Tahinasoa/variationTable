13358721c4b268d1af15dffc02c2e6f77d335319
Tahinasoa <122441619+Tahinasoa@users.noreply.github.com>
2026-03-03 20:53:41 +0300

    update examples

--- src/examples.ts
+++ src/examples.ts
@@ -5,29 +5,40 @@ row "sign of $f(x)$": + |&{center:$0$} - |&{center:$0$} -
 row "Variations de $\\sqrt{x^2-1}$"/2: &{top-right:$+\\infty$} DEC &{bottom-left:$-\\infty$} UNDEF &{bottom-right:$+\\infty$}INC &{top-left:$+\\infty$}
 row $g'(x)$/2 : &{bottom-right:$-5$} INC |&{top:$-2$} CONST |&{top-left :$-2$, bottom-right:$-\\infty$}INC &{top-left:$-1$}`;
 
-examples['2.1'] = `header : $-\\infty$,$0$,$+\\infty$
-row "signe de $f'$": + :& -
-row "variation de $f(x)$": INC DEC`;
+examples["2.1"] = 
+`header $x$ : $-\\infty$, $0$, $+\\infty$`
 
-examples['3.1'] = `header $x$ : $-\\infty$,$0$,$+\\infty$
-row "signe de $f'$": + -
-row "variation de $f(x)$": INC &{top-center:"5", bottom-center:"4"}DEC `;
+examples["2.2"] = 
+`header $x$ : $-\\infty$, $0$, $+\\infty$
+row "sign of $f'(x)$" /1.5 : + -`
 
-examples['3.2'] = `header : "", "" , "","" , "" 
-row /2: INC CONST{arrow:"both"} DEC{arrow:"start"} INC{arrow:"end"}
-row /2: INC{colSpan:2} DEC {colSpan:2}`;
+examples["3.1"] = 
+`header : $-1$,$0$, $+1$
+row  : |& + ::& - |&
+row  : ||& - |& + ||&`
 
-examples['4.1'] =
-  //row is skipped
-  `header : "", "" , "", ""
-row : INC |&  |& DEC`;
 
-examples['4.2'] =
-  //multiple label attached to a separator
-  `header : "", "" , ""
-row/2 : INC |&{top-center:$+1$, bottom-center:$-1$} INC`;
+examples["3.2"] = 
+`header : $-1$,$0$, $+1$
+row : + |&{top-left:"top-left", bottom-right:"bottom-right"} -
+row : + |&{center:$0$} -`
 
-examples['4.3'] =
-  //mixed content type
-  `header : "", "" , "", ""
-row/2 : INC |& "I don't know" |& DEC`;
+examples["4.1"] = 
+`header : $-\\infty$,$-1$, $+1$,$+\\infty$
+row : + UNDEF +
+row : UNDEF SKIP UNDEF`
+examples["5.1"] = 
+`header : $-\\infty$,$-1$, $+1$,$+\\infty$
+row : INC CONST DEC`
+
+examples["5.2"] = 
+`header : $-\\infty$,$-1$, $+1$,$+\\infty$
+row : INC{arrow:"start"} CONST{arrow:"none", vPos:"bottom"} DEC{arrow:"both"}`
+examples["5.3"] = 
+`header : $-\\infty$,$0$,$+\\infty$
+row/2 : &{bottom-right:$-\\infty$} INC |&{top:$-1$} DEC &{bottom-left:$-\\infty$}`
+examples["5.4"] = 
+`header : $-\\infty$,$-1$,$+1$, $+\\infty$
+row : "text"{colSpan:2} INC
+      row : UNDEF{colSpan:3}
+      row : DEC{arrow:"none", colSpan:2} CONST`
